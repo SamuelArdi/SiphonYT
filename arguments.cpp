7300655d8741edc787bcc145b15ea11e6539bd7c
SamuelArdi <149386215+SamuelArdi@users.noreply.github.com>
2025-05-09 19:23:57 +0800

    now using CMake for easier building

---
+++ arguments.cpp
@@ -0,0 +1,90 @@
+#include "arguments.h"
+#include <vector>
+
+// required functions
+std::string lowerStr(std::vector<std::string> args, int valueIndex) {
+  std::string lowercase;
+  for (char c : args[valueIndex]) {
+    lowercase += std::tolower(c);
+  }
+
+  return lowercase;
+}
+
+int getValueIndex(std::vector<std::string> vec, std::string arg) {
+  auto id = std::find(vec.begin(), vec.end(), arg);
+  if (id != vec.end()) {
+    return std::distance(vec.begin(), id) + 1;
+  } else {
+    std::cerr << "Not found";
+    return 404;
+  }
+}
+
+// single arguments
+void showHelp() {
+  std::cout << "siphonyt [Arguments] [video/playlist url]" << std::endl;
+
+  std::cout << "\nArguments:" << std::endl;
+  std::cout << "-h                  Print this help text and exit" << std::endl;
+  std::cout << "-v                  Print program version and exit"
+            << std::endl;
+  std::cout << "-x                  Extract audio (no video)" << std::endl;
+  std::cout<< "-f <audio format>    Convert to selected audio format (default: opus)" << std::endl;
+}
+
+void showVersion() {
+  std::cout << "SiphonYT pre-alpha testing" << std::endl;
+  std::cout << "Version: 0.1.6.alpha" << std::endl;
+}
+
+void extractAudio(std::string cmd) {
+  cmd += "-x";
+  cmd += " ";
+}
+
+void keepOrgFile(std::string cmd) {
+  cmd += "-k";
+  cmd += " ";
+}
+
+
+// value arguments
+void formatArgument(std::vector<std::string> args, std::string &cmd,
+                    bool isLong) {
+  int valueIndex;
+  if (isLong == true) {
+    valueIndex = getValueIndex(args, "--format");
+  } else {
+    valueIndex = getValueIndex(args, "-f");
+  }
+
+  std::string value = lowerStr(args, valueIndex);
+#ifdef _WIN32
+  cmd += "-S res,ext;" + value + " --recode " + value;
+  cmd += " ";
+#elifdef unix
+  cmd += "-S 'res,ext:" + value + "' --recode '" + value + "'";
+  cmd += " ";
+#elifndef
+#endif
+}
+
+void qualityArgument(std::vector<std::string> args, std::string &cmd, bool isLong) {
+  int valueIndex;
+  if (isLong == true) {
+    valueIndex = getValueIndex(args, "--quality");
+  } else {
+    valueIndex = getValueIndex(args, "-q");
+  }
+
+  std::string value = lowerStr(args, valueIndex);
+#ifdef _WIN32
+  cmd += "-S height:" + value + " -f bv*+ba/b";
+  cmd += " ";
+#elifdef unix
+  cmd += "-S 'height:" + value + "' -f 'bva*+ba/b'";
+  cmd += " ";
+#elifndef
+#endif
+}
---
+++ main.cpp
@@ -0,0 +1,97 @@
+#include <algorithm>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "arguments.h"
+
+/*
+int stoa(std::string str) {
+  // converts string into ascii numbers
+
+  std::string ascii = "";
+  for (int i = 0; i < str.length(); i++) {
+    ascii += std::to_string(static_cast<int>(str[i]));
+  }
+
+  return std::stoi(ascii);
+}
+*/
+
+bool findVecElem(std::vector<std::string> vec, std::string elemToFind) {
+  auto elem = std::find(vec.begin(), vec.end(), elemToFind);
+  if (elem != vec.end()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void handler(std::vector<std::string> args, bool &exit, std::string &command) {
+  // single arguments
+  if (findVecElem(args, "-h") == true || findVecElem(args, "--help") == true) {
+    showHelp();
+    exit = true;
+    return;
+  }
+
+  if (findVecElem(args, "-v") == true ||
+      findVecElem(args, "--version") == true) {
+    showVersion();
+    exit = true;
+    return;
+  }
+
+  if (findVecElem(args, "-x") == true) {
+    extractAudio(command);
+  }
+
+  // value arguments
+  if (findVecElem(args, "-f") == true) {
+    formatArgument(args, command, false);
+  } else if (findVecElem(args, "--format") == true) {
+    formatArgument(args, command, true);
+  }
+
+  if (findVecElem(args, "-q") == true) {
+    qualityArgument(args, command, false);
+  } else if (findVecElem(args, "--quality") == true) {
+    qualityArgument(args, command, true);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cout << "No URL present, type '-h' for help" << std::endl;
+    return 0;
+  }
+
+  std::vector<std::string> args;
+  for (int i = 1; i < argc; i++) {
+    args.push_back(argv[i]);
+  }
+
+#ifdef _WIN32
+  std::string url = std::string(argv[argc - 1]);
+#elifdef unix
+  std::string url = "'" + std::string(argv[argc - 1]) + "'";
+#elifndef
+#endif
+  std::string command = "yt-dlp";
+  command += " ";
+
+  bool exitCli = false;
+  handler(args, exitCli, command);
+  command += url;
+
+  if (exitCli == true) {
+    return 0;
+  }
+
+  //std::cout << command;
+  system(command.c_str());
+  return 0;
+}
