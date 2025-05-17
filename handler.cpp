e434389b8c8e882b809c7d3101af7aa35fcebbf3
SamuelArdi <149386215+SamuelArdi@users.noreply.github.com>
2025-05-17 19:00:12 +0800

    final commit of the day. handler function now has a separate file for easier managing.

--- handler.cpp
+++ handler.cpp
@@ -1,10 +1,19 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
 #include "handler.h"
+#include "arguments.h"
 
 /*
+int stoa(std::string str) {
+  // converts string into ascii numbers
+
+  std::string ascii = "";
+  for (int i = 0; i < str.length(); i++) {
+    ascii += std::to_string(static_cast<int>(str[i]));
+  }
+
+  return std::stoi(ascii);
+}
+*/
+
 bool findVecElem(std::vector<std::string> vec, std::string elemToFind) {
   auto elem = std::find(vec.begin(), vec.end(), elemToFind);
   if (elem != vec.end()) {
@@ -89,44 +98,4 @@ void handler(std::vector<std::string> args, bool &exit, std::string &command) {
   if (findVecElem(args, "--sub-langs") == true) {
     subLangs(args, command, true);
   }
-}
-*/
-
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    std::cout << "No URL present, type '-h' for help" << std::endl;
-    return 0;
-  }
-
-  std::vector<std::string> args;
-  for (int i = 1; i < argc; i++) {
-    args.push_back(argv[i]);
-  }
-
-#ifdef _WIN32
-  std::string url = "\"" + std::string(argv[argc - 1]) + "\"";
-#elifdef unix
-  std::string url = "'" + std::string(argv[argc - 1]) + "'";
-#elifndef
-#endif
-  std::string command = "yt-dlp";
-  command += " ";
-
-  bool exitCli = false;
-  handler(args, exitCli, command);
-  command += url;
-  
-  // debugging
-  for (int i = 0; i < args.size(); i++) {
-    std::cout << "Arg " << i + 1 << ": " << args[i] << "\n"; // "\n" instead of std::endl for performance
-  }
-  std::cout << command << std::endl;
-
-  if (exitCli == true) {
-    return 0;
-  }
-
-  // command activation
-  system(command.c_str());
-  return 0;
 }
---
+++ handler.h
@@ -0,0 +1,9 @@
+#ifndef HANDLER_H
+#define HANDLER_H
+
+#include <vector>
+#include <string>
+
+void handler(std::vector<std::string> args, bool &exit, std::string &command);
+
+#endif // HANDLER_H
